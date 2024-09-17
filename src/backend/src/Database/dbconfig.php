1129b74cc48eecb91033224840937cd87e9ba356
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-17 10:28:02 +0200

    Remove backend

--- src/backend/src/Database/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-DEFINE('DB_USER', 'doadmin');
-DEFINE('DB_PASSWORD', 'AVNS_em7DPpWxsGi8nyllhjS');
-DEFINE('DB_HOST', 'db-mysql-fra1-72913-do-user-16822868-0.l.db.ondigitalocean.com');
-DEFINE('DB_NAME', 'defaultdb');
-DEFINE('DB_PORT', '25060');
-// did not use env vars this time
