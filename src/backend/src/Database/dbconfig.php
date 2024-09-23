76d75f20a97fa937d45c0206cd67c51da1819c02
Anur Peljto <116578425+anurpeljto@users.noreply.github.com>
2024-09-23 16:42:09 +0200

    Update dbconfig.php

--- src/backend/src/Database/dbconfig.php
+++ src/backend/src/Database/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 
-DEFINE('DB_USER', 'doadmin');
-DEFINE('DB_PASSWORD', 'AVNS_em7DPpWxsGi8nyllhjS');
-DEFINE('DB_HOST', 'db-mysql-fra1-72913-do-user-16822868-0.l.db.ondigitalocean.com');
+DEFINE('DB_USER', 'avnadmin');
+DEFINE('DB_PASSWORD', 'AVNS_A6DNyAgtL9KRyb3ixNo');
+DEFINE('DB_HOST', 'mysql-36233449-stu-e988.h.aivencloud.com');
 DEFINE('DB_NAME', 'defaultdb');
-DEFINE('DB_PORT', '25060');
+DEFINE('DB_PORT', '25383');
 // did not use env vars this time
